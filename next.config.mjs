d61a55e5b6adb6ed0825d15f72317369dd0bc31b
kp1153 <kamta_tiwari@yahoo.com>
2025-10-12 16:15:39 +0530

    Fixed some issues

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,24 @@ const nextConfig = {
         protocol: "https",
         hostname: "cdn.sanity.io",
       },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
     ],
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.optimization.splitChunks.cacheGroups = {
+        ...config.optimization.splitChunks.cacheGroups,
+        sanity: {
+          test: /[\\/]node_modules[\\/]sanity[\\/]/,
+          name: "sanity",
+          priority: 10,
+        },
+      };
+    }
+    return config;
   },
 };
 
