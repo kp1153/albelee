0b8ad5f7a142b3f1d1cd02d9c5c147e7a2a36498
kp1153 <kamta_tiwari@yahoo.com>
2025-10-15 10:18:52 +0530

    Fixed some issues

--- sanity.config.js
+++ sanity.config.js
@@ -1,13 +1,12 @@
 "use client";
 
 /**
- * This configuration is used to for the Sanity Studio that's mounted on the `\app\studio\[[...tool]]\page.jsx` route
+ * This configuration is used to for the Sanity Studio that’s mounted on the `\app\studio\[[...tool]]\page.jsx` route
  */
 
 import { visionTool } from "@sanity/vision";
 import { defineConfig } from "sanity";
 import { structureTool } from "sanity/structure";
-import { cloudinarySchemaPlugin } from "sanity-plugin-cloudinary";
 
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
 import { apiVersion, dataset, projectId } from "./sanity/env";
@@ -25,6 +24,5 @@ export default defineConfig({
     // Vision is for querying with GROQ from inside the Studio
     // https://www.sanity.io/docs/the-vision-plugin
     visionTool({ defaultApiVersion: apiVersion }),
-    cloudinarySchemaPlugin(),
   ],
 });
